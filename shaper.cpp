eecb6f201da18afe5ef235d9b13f85fc148e6b65
AMDIvailo <ipanamski@gmail.com>
2016-02-21 11:42:04 +0200

    Remove example use

--- shaper.cpp
+++ shaper.cpp
@@ -145,12 +145,3 @@ public:
     isSolid=solid;
   }
 }; 
-
-int main()
-{
-  shaper myshape("rectangle");
-  myshape.setSymbol('1');
-  myshape.setSize(10, 100);
-  myshape.setSolid(true);
-  myshape.draw();
-}
