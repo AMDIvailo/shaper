853362d9a95660d73c9fb643472efbeb47bd0f31
AMDIvailo <ipanamski@gmail.com>
2016-02-21 11:22:28 +0200

    Update shaper.cpp

--- shaper.cpp
+++ shaper.cpp
@@ -1,4 +1,4 @@
-//This file is part of Foobar.
+//This file is part of Shaper.
 
 //Shaper is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
